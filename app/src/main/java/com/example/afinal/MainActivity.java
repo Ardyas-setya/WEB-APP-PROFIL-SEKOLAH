e6cb9e0fff3b18cf233ebe2aa857ee588e46d4a8
Firdan Mahendra Wardana <72062343+firdanmahendraa@users.noreply.github.com>
2022-05-31 16:20:20 +0700

    home screen update

--- app/src/main/java/com/example/afinal/MainActivity.java
+++ app/src/main/java/com/example/afinal/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.afinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.WindowManager;
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +16,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageSlider imageSlider = findViewById(R.id.slider);
+
+        List<SlideModel> slideModels = new ArrayList<>();
+
+        slideModels.add(new SlideModel("https://picsum.photos/id/237/200/300", "Title", ScaleTypes.FIT));
+        slideModels.add(new SlideModel("https://picsum.photos/seed/picsum/200/300", "Title", ScaleTypes.FIT));
+        slideModels.add(new SlideModel("https://picsum.photos/200/300?grayscale", "Title", ScaleTypes.FIT));
+        slideModels.add(new SlideModel("https://picsum.photos/200/300/?blur", "Title", ScaleTypes.FIT));
+
+        imageSlider.setImageList(slideModels, ScaleTypes.CENTER_CROP);
+
 
     }
+
+
 }
